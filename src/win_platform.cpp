8d23c3ad9556bcd34e6b184ef2afa846111ba797
ryszardzmija <ryszard_zmija_dev@outlook.com>
2025-12-26 12:37:16 +0100

    Added repaint after resize and cleaned up the code. Added update() for interfacing with the renderer.

--- src/win_platform.cpp
+++ src/win_platform.cpp
@@ -2,11 +2,16 @@
 
 constexpr const auto WC_NAME = TEXT("rendered_window");
 constexpr const auto WIN_TITLE = TEXT("Rendered Window");
-constexpr int WIN_WIDTH = 640;
-constexpr int WIN_HEIGHT = 480;
+constexpr int INIT_WIDTH = 640;
+constexpr int INIT_HEIGHT = 480;
 
-void* pixel_buf;
-HDC mem_dc;
+typedef struct WindowState_ {
+	HDC mem_dc;
+	void* pixel_buf;
+	HBITMAP dib;
+	LONG width;
+	LONG height;
+} WindowState;
 
 static void registerWindowClass(WNDPROC win_proc_func, HINSTANCE h_instance) {
 	WNDCLASS wc = {};
@@ -23,30 +28,14 @@ static void registerWindowClass(WNDPROC win_proc_func, HINSTANCE h_instance) {
 	RegisterClass(&wc);
 }
 
-static void fillBlue(void* buf) {
-	UINT32* pixels =  static_cast<UINT32*>(buf);
-	for (int i = 0; i < WIN_WIDTH * WIN_HEIGHT; ++i) {
-		pixels[i] = 0x000000FF;
+void createPixelBuffer(LONG width, LONG height, WindowState* win_state) {
+	if (win_state->dib) {
+		DeleteObject(win_state->dib);
 	}
-}
-
-LRESULT CALLBACK windowProc(HWND hwnd, UINT u_msg, WPARAM w_param, LPARAM l_param) {
-	switch (u_msg) {
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		return 0;
-	case WM_PAINT:
-		PAINTSTRUCT ps;
-		BeginPaint(hwnd, &ps);
-		BitBlt(ps.hdc, 0, 0, WIN_WIDTH, WIN_HEIGHT, mem_dc, 0, 0, SRCCOPY);
-		EndPaint(hwnd, &ps);
-		return 0;
-	default:
-		return DefWindowProc(hwnd, u_msg, w_param, l_param);
+	if (win_state->mem_dc) {
+		DeleteDC(win_state->mem_dc);
 	}
-}
 
-void createPixelBuffer(LONG width, LONG height, void** pixel_buf, HDC* mem_dc) {
 	BITMAPINFOHEADER header = {};
 	header.biSize = sizeof(BITMAPINFOHEADER);
 	header.biWidth = width;
@@ -60,25 +49,80 @@ void createPixelBuffer(LONG width, LONG height, void** pixel_buf, HDC* mem_dc) {
 
 	HBITMAP dib = CreateDIBSection(
 		nullptr, &info, DIB_RGB_COLORS,
-		pixel_buf, nullptr, 0
+		&win_state->pixel_buf, nullptr, 0
 	);
 	HDC dc = CreateCompatibleDC(nullptr);
 	SelectObject(dc, dib);
-	*mem_dc = dc;
+	win_state->mem_dc = dc;
+	win_state->dib = dib;
+	win_state->width = width;
+	win_state->height = height;
+}
+
+static void fillBlue(void* buf, int width, int height) {
+	UINT32* pixels =  static_cast<UINT32*>(buf);
+	for (int i = 0; i < width * height; ++i) {
+		pixels[i] = 0x000000FF;
+	}
+}
+
+static void update(void* buf, int width, int height) {
+	fillBlue(buf, width, height);
+}
+
+static void draw(HWND hwnd, HDC mem_dc, int width, int height) {
+	PAINTSTRUCT ps;
+	BeginPaint(hwnd, &ps);
+	BitBlt(ps.hdc, 0, 0, width, height, mem_dc, 0, 0, SRCCOPY);
+	EndPaint(hwnd, &ps);
+}
+
+LRESULT CALLBACK windowProc(HWND hwnd, UINT u_msg, WPARAM w_param, LPARAM l_param) {
+	if (u_msg == WM_NCCREATE) {
+		// Attach window state data to the window
+		CREATESTRUCT* create_params = reinterpret_cast<CREATESTRUCT*>(l_param);
+		WindowState* win_state = static_cast<WindowState*>(create_params->lpCreateParams);
+		SetWindowLongPtr(hwnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(win_state));
+		return TRUE;
+	}
+
+	WindowState* ws = reinterpret_cast<WindowState*>(GetWindowLongPtr(hwnd, GWLP_USERDATA));
+
+	int width;
+	int height;
+	switch (u_msg) {
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	case WM_PAINT:
+		width = ws->width;
+		height = ws->height;
+		update(ws->pixel_buf, width, height);
+		draw(hwnd, ws->mem_dc, width, height);
+		return 0;
+	case WM_SIZE:
+		if (LOWORD(l_param) > 0 && HIWORD(l_param) > 0) {
+			createPixelBuffer(LOWORD(l_param), HIWORD(l_param), ws);
+		}
+		DefWindowProc(hwnd, u_msg, w_param, l_param);
+		InvalidateRect(hwnd, NULL, FALSE);
+		return 0;
+	default:
+		return DefWindowProc(hwnd, u_msg, w_param, l_param);
+	}
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int cmdShow) {
 	registerWindowClass(windowProc, hInstance);
+	WindowState win_state = {};
+
 	HWND window = CreateWindow(
 		WC_NAME, WIN_TITLE,
 		WS_OVERLAPPEDWINDOW | WS_VISIBLE,
 		CW_USEDEFAULT, CW_USEDEFAULT,
-		WIN_WIDTH, WIN_HEIGHT,
-		NULL, NULL, hInstance, NULL
+		INIT_WIDTH, INIT_HEIGHT,
+		NULL, NULL, hInstance, &win_state
 	);
-	
-	createPixelBuffer(WIN_WIDTH, WIN_HEIGHT, &pixel_buf, &mem_dc);
-	fillBlue(pixel_buf);
 
 	MSG msg;
 	while (GetMessage(&msg, NULL, 0, 0)) {
@@ -86,5 +130,5 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		DispatchMessage(&msg);
 	}
 
-	return msg.wParam;
+	return static_cast<int>(msg.wParam);
 }
